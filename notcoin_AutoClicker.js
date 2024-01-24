bb93160c1a0953dbbd613ad92335ea56f43ce09d
omidrr <64539596+omidRR@users.noreply.github.com>
2024-01-24 22:00:24 +0330

    Create notcoin_AutoClicker.js

---
+++ notcoin_AutoClicker.js
@@ -0,0 +1,53 @@
+function autoClicker() {
+    let powerLimitForAutotap = 100;
+    let countclicks = 34;
+    let recharging = false;
+    let skipClick = false;
+
+    let app_root = document.querySelector('div[class^="_root"]');
+    let multipleClicks = 45;
+    if (multipleClicks === undefined || multipleClicks === null) { multipleClicks = 0; }
+
+    async function click() {
+        let cc = document.querySelectorAll('div[class^="_notcoin"]');
+        let scoreElement = document.querySelector('div[class^="_scoreCurrent"]');
+        let score = parseInt(scoreElement.textContent);
+
+        try {
+            let imrocket = document.querySelectorAll('img[class^="_root"]');
+            imrocket[0][Object.keys(imrocket[0])[1]].onClick();
+            recharging = false;
+        } catch (error) {}
+
+        for (let step = 0; step < countclicks; step++) {
+            score = parseInt(scoreElement.textContent);
+
+            if (skipClick) {
+                break;
+            }
+
+            if (recharging) {
+                if (score >= powerLimitForAutotap) {
+                    recharging = false;
+                }
+                break;
+            }
+
+            if (score > multipleClicks * 2) {
+                try {
+                    await new Promise((resolve) => {
+                        cc[0][Object.keys(cc[0])[1]].onTouchStart('');
+                        setTimeout(resolve, 100);
+                    });
+                } catch (error) {}
+            } else {
+                recharging = true;
+                break;
+            }
+        }
+    }
+
+    setInterval(click, 350);
+}
+
+autoClicker();
